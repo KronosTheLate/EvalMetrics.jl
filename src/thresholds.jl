edecee5496a027cd447ad64e96d3f75134f060d5
Vaclav Macha <vaclav.macha92@gmail.com>
2020-08-10 12:43:56 +0200

    Added docs strings to thresholds.jl

--- src/thresholds.jl
+++ src/thresholds.jl
@@ -1,29 +1,93 @@
 """
-    $(SIGNATURES)
+    thresholds(scores::RealVector, n::Int = length(scores))
 
 Returns `n` decision thresholds which correspond to `n` evenly spaced quantiles
-of the given vector of scores. If `reduced == true`, then the resulting `n` is
-`min(length(scores) + 1, n)`. If `zerorecall == true`, then the largest threshold
-will be `maximum(scores)*(1 + eps())` otherwise `maximum(scores)`.
+of the given vector of scores.
+
+# Keyword arguments
+
+- `reduced::Bool = true`: if `true` the resulting number of thresholds is
+`min(length(scores), n)`
+- `zerorecall::Bool = true`: if `true` the resulting number of thresholds is
+`n + 1` and the largest threshold is `maximum(scores) + eps()`
+
+# Examples
+
+```jldoctest
+julia> thresholds(0:1:3, 5; reduced = true, zerorecall = true)
+5-element Array{Float64,1}:
+ 0.0
+ 1.0
+ 2.0
+ 3.0
+ 3.0000000000000004
+
+julia> thresholds(0:1:3, 5; reduced = false, zerorecall = true)
+6-element Array{Float64,1}:
+ 0.0
+ 0.75
+ 1.5
+ 2.25
+ 3.0
+ 3.0000000000000004
+
+julia> thresholds(0:1:3, 5; reduced = true, zerorecall = false)
+4-element Array{Float64,1}:
+ 0.0
+ 1.0
+ 2.0
+ 3.0
+
+julia> thresholds(0:1:3, 5; reduced = false, zerorecall = false)
+5-element Array{Float64,1}:
+ 0.0
+ 0.75
+ 1.5
+ 2.25
+ 3.0
+```
 """
 function thresholds(
     scores::RealVector,
-    n::Int = length(scores) + 1;
+    n::Int = length(scores);
     reduced::Bool = true,
     zerorecall::Bool = true,
 )
 
-    ns = length(scores)
-    N = reduced ? min(ns + 1, n) : n
-    N -= zerorecall
-    if N == ns
-        thres = sort(scores)
+    N = reduced ? min(length(scores), n) : n
+    thres = quantile(scores, range(0, 1, length = N))
+    if zerorecall
+        val = eltype(thres)(maximum(scores))
+        vcat(thres, val + eps(val))
     else
-        thres = quantile(scores, range(0, 1, length = N))
+        thres
     end
-    return zerorecall ? vcat(thres, maximum(scores) + eps()) : thres
 end
 
+
+"""
+    threshold_at_k(scores::RealVector, k::Int; rev::Bool = true)
+
+If `rev=true` return `k`-th largest score and `k`-th smallest score otherwise.
+
+# Examples
+
+```jldoctest
+julia> threshold_at_k(0:1:10, 3)
+8
+
+julia> threshold_at_k(0:1:10, 3; rev = false)
+2
+```
+"""
+function threshold_at_k(scores::RealVector, k::Int; rev::Bool = true)
+
+    n = length(scores)
+    if n < k
+        throw(ArgumentError("`k` must be smaller or equal to `length(scores) = $(n)`."))
+    end
+    return partialsort(scores, k, rev = rev)
+end
 
 function threshold_at_rate(scores::RealVector, rates::RealVector; rev::Bool = true)
 
@@ -70,10 +134,38 @@ function threshold_at_rate(scores::RealVector, rates::RealVector; rev::Bool = tr
 end
 
 
-"""
-    $(SIGNATURES)
+@doc raw"""
+    threshold_at_tpr(targets, scores, tpr)
+    threshold_at_tpr(enc, targets, scores, tpr)
 
-Returns a decision threshold at a given true positive rate `tpr ∈ [0, 1]`.
+For given true positive rate `tpr ∈ [0, 1]`, return the highest possible decision threshold `t` that satisfies
+
+```math
+\mathrm{true\_positive\_rate}(targets, scores, t) \geq tpr > \mathrm{true\_positive\_rate}(targets, scores, t + \varepsilon)
+```
+
+# Arguments
+
+- `targets::AbstractVector`: ground truth (correct) target values
+- `scores::RealVector`: a vector of scores given by the classifier; a sample `i` is classified as positive if `scores[i] >= t`, where `t` is the decision threshold
+- `tpr::Union{Real, RealVector}`: true positive rate or a sorted vector of true poitive rates; if `tpr` is scalar the function returns one threshold, otherwise it returns a vector of thresholds
+- `enc::TwoClassEncoding`: label encoding for two-class problems; the default encoding is given by [`current_encoding`](@ref) function
+
+# Examples
+```jldoctest
+julia> targets = [0, 1, 1, 0, 1, 0, 1, 1, 0, 1];
+
+julia> scores = [0.2, 0.7, 0.3, 0.6, 0.8, 0.4, 0.3, 0.5, 0.7, 0.9];
+
+julia> t = threshold_at_tpr(targets, scores, 0.5)
+0.7
+
+julia> true_positive_rate(targets, scores, t)
+0.5
+
+julia> true_positive_rate(targets, scores, t + eps(t))
+0.3333333333333333
+```
 """
 function threshold_at_tpr(targets::AbstractVector, scores::RealVector, tpr)
     return threshold_at_tpr(current_encoding(), targets, scores, tpr)
@@ -116,11 +208,38 @@ function threshold_at_tpr(
     return reverse(ts)
 end
 
+@doc raw"""
+    threshold_at_tnr(targets, scores, tnr)
+    threshold_at_tnr(enc, targets, scores, tnr)
 
-"""
-    $(SIGNATURES)
+For given true negative rate `tnr ∈ [0, 1]`, return the smallest possible decision threshold `t` that satisfies
 
-Returns a decision threshold at a given true negative rate `fpr ∈ [0, 1]`.
+```math
+\mathrm{true\_negative\_rate}(targets, scores, t) \geq tnr > \mathrm{true\_negative\_rate}(targets, scores, t - \varepsilon)
+```
+
+# Arguments
+
+- `targets::AbstractVector`: ground truth (correct) target values
+- `scores::RealVector`: a vector of scores given by the classifier; a sample `i` is classified as positive if `scores[i] >= t`, where `t` is the decision threshold
+- `tnr::Union{Real, RealVector}`: true positive rate or a sorted vector of true poitive rates; if `tnr` is scalar the function returns one threshold, otherwise it returns a vector of thresholds
+- `enc::TwoClassEncoding`: label encoding for two-class problems; the default encoding is given by [`current_encoding`](@ref) function
+
+# Examples
+```jldoctest
+julia> targets = [0, 1, 1, 0, 1, 0, 1, 1, 0, 1];
+
+julia> scores = [0.2, 0.7, 0.3, 0.6, 0.8, 0.4, 0.3, 0.5, 0.7, 0.9];
+
+julia> t = threshold_at_tnr(targets, scores, 0.5)
+0.4000000000000001
+
+julia> true_negative_rate(targets, scores, t)
+0.5
+
+julia> true_negative_rate(targets, scores, t - eps(t))
+0.25
+```
 """
 function threshold_at_tnr(targets::AbstractVector, scores::RealVector, tnr)
     return threshold_at_tnr(current_encoding(), targets, scores, tnr)
@@ -163,11 +282,38 @@ function threshold_at_tnr(
     return reverse(ts)
 end
 
+@doc raw"""
+    threshold_at_fpr(targets, scores, tnr)
+    threshold_at_fpr(enc, targets, scores, tnr)
 
-"""
-    $(SIGNATURES)
+For given false positive rate `tnr ∈ [0, 1]`, return the smallest possible decision threshold `t` that satisfies
 
-Returns a decision threshold at a given false positive rate `fpr ∈ [0, 1]`.
+```math
+\mathrm{false\_positive\_rate}(targets, scores, t) \geq fpr < \mathrm{false\_positive\_rate}(targets, scores, t - \varepsilon)
+```
+
+# Arguments
+
+- `targets::AbstractVector`: ground truth (correct) target values
+- `scores::RealVector`: a vector of scores given by the classifier; a sample `i` is classified as positive if `scores[i] >= t`, where `t` is the decision threshold
+- `fpr::Union{Real, RealVector}`: true positive rate or a sorted vector of true poitive rates; if `fpr` is scalar the function returns one threshold, otherwise it returns a vector of thresholds
+- `enc::TwoClassEncoding`: label encoding for two-class problems; the default encoding is given by [`current_encoding`](@ref) function
+
+# Examples
+```jldoctest
+julia> targets = [0, 1, 1, 0, 1, 0, 1, 1, 0, 1];
+
+julia> scores = [0.2, 0.7, 0.3, 0.6, 0.8, 0.4, 0.3, 0.5, 0.7, 0.9];
+
+julia> t = threshold_at_fpr(targets, scores, 0.5)
+0.4000000000000001
+
+julia> false_positive_rate(targets, scores, t)
+0.5
+
+julia> false_positive_rate(targets, scores, t - eps(t))
+0.75
+```
 """
 function threshold_at_fpr(targets::AbstractVector, scores::RealVector, fpr)
     return threshold_at_fpr(current_encoding(), targets, scores, fpr)
@@ -208,11 +354,38 @@ function threshold_at_fpr(
     return ts
 end
 
+@doc raw"""
+    threshold_at_fnr(targets, scores, fnr)
+    threshold_at_fnr(enc, targets, scores, fnr)
 
-"""
-    $(SIGNATURES)
+For given false positive rate `fnr ∈ [0, 1]`, return the highest possible decision threshold `t` that satisfies
 
-Returns a decision threshold at a given false negative rate `fnr ∈ [0, 1]`.
+```math
+\mathrm{false\_negative\_rate}(targets, scores, t) \geq fnr < \mathrm{false\_negative\_rate}(targets, scores, t + \varepsilon)
+```
+
+# Arguments
+
+- `targets::AbstractVector`: ground truth (correct) target values
+- `scores::RealVector`: a vector of scores given by the classifier; a sample `i` is classified as positive if `scores[i] >= t`, where `t` is the decision threshold
+- `fnr::Union{Real, RealVector}`: true positive rate or a sorted vector of true poitive rates; if `fnr` is scalar the function returns one threshold, otherwise it returns a vector of thresholds
+- `enc::TwoClassEncoding`: label encoding for two-class problems; the default encoding is given by [`current_encoding`](@ref) function
+
+# Examples
+```jldoctest
+julia> targets = [0, 1, 1, 0, 1, 0, 1, 1, 0, 1];
+
+julia> scores = [0.2, 0.7, 0.3, 0.6, 0.8, 0.4, 0.3, 0.5, 0.7, 0.9];
+
+julia> t = threshold_at_fnr(targets, scores, 0.5)
+0.7
+
+julia> false_negative_rate(targets, scores, t)
+0.5
+
+julia> false_negative_rate(targets, scores, t + eps(t))
+0.6666666666666666
+```
 """
 function threshold_at_fnr(targets::AbstractVector, scores::RealVector, fnr)
     return threshold_at_fnr(current_encoding(), targets, scores, fnr)
@@ -251,20 +424,4 @@ function threshold_at_fnr(
         @warn "The closest lower feasible false negative rate to some of the required values ($(join(rates, ", "))) is 0.0!"
     end
     return ts
-end
-
-
-"""
-    $(SIGNATURES)
-
-Returns a decision threshold at `k` most anomalous samples if `rev == true` and
-a decision threshold at `k` least anomalous samples otherwise.
-"""
-function threshold_at_k(scores::RealVector, k::Int; rev::Bool = true)
-
-    n = length(scores)
-    if n < k
-        throw(ArgumentError("`k` must be smaller or equal to `length(targets) = $(n)`"))
-    end
-    return partialsort(scores, k, rev = rev)
 end
