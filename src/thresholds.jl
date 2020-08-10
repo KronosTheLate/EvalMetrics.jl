e0f742955355d89b32596ea18ceffb7bc3683413
Vaclav Macha <vaclav.macha92@gmail.com>
2020-08-10 11:19:05 +0200

    Refactores thresholds.jl

--- src/thresholds.jl
+++ src/thresholds.jl
@@ -6,7 +6,13 @@ of the given vector of scores. If `reduced == true`, then the resulting `n` is
 `min(length(scores) + 1, n)`. If `zerorecall == true`, then the largest threshold
 will be `maximum(scores)*(1 + eps())` otherwise `maximum(scores)`.
 """
-function thresholds(scores::RealVector, n::Int = length(scores) + 1; reduced::Bool = true, zerorecall::Bool = true)
+function thresholds(
+    scores::RealVector,
+    n::Int = length(scores) + 1;
+    reduced::Bool = true,
+    zerorecall::Bool = true,
+)
+
     ns = length(scores)
     N = reduced ? min(ns + 1, n) : n
     N -= zerorecall
@@ -15,13 +21,11 @@ function thresholds(scores::RealVector, n::Int = length(scores) + 1; reduced::Bo
     else
         thres = quantile(scores, range(0, 1, length = N))
     end
-    zerorecall && push!(thres, maximum(scores) + eps())
-
-    return thres
+    return zerorecall ? vcat(thres, maximum(scores) + eps()) : thres
 end
 
 
-function threshold_at_rate(enc::TwoClassEncoding, scores::RealVector, rates::RealVector; rev::Bool = true)
+function threshold_at_rate(scores::RealVector, rates::RealVector; rev::Bool = true)
 
     all(0 .<= rates .<= 1) || throw(ArgumentError("input rates out of [0, 1]."))
     issorted(rates) || throw(ArgumentError("input rates must be sorted."))
@@ -71,22 +75,39 @@ end
 
 Returns a decision threshold at a given true positive rate `tpr ∈ [0, 1]`.
 """
-threshold_at_tpr(targets::AbstractVector, scores::RealVector, tpr) =
-    threshold_at_tpr(current_encoding(), targets, scores, tpr)
+function threshold_at_tpr(targets::AbstractVector, scores::RealVector, tpr)
+    return threshold_at_tpr(current_encoding(), targets, scores, tpr)
+end
 
 
-threshold_at_tpr(enc::TwoClassEncoding, targets::AbstractVector, scores::RealVector, tpr::Real) =
-    threshold_at_tpr(enc, targets, scores, [tpr])[1]
+function threshold_at_tpr(
+    enc::TwoClassEncoding,
+    targets::AbstractVector,
+    scores::RealVector,
+    tpr::Real,
+)
+
+    return threshold_at_tpr(enc, targets, scores, [tpr])[1]
+end
 
 
-function threshold_at_tpr(enc::TwoClassEncoding, targets::AbstractVector, scores::RealVector, tpr::RealVector)
+function threshold_at_tpr(
+    enc::TwoClassEncoding,
+    targets::AbstractVector,
+    scores::RealVector,
+    tpr::RealVector,
+)
 
-    length(targets) == length(scores) || throw(DimensionMismatch("Inconsistent lengths of `targets` and `scores`."))
-    check_encoding(enc, targets) || throw(ArgumentError("`targets` vector uses incorrect label encoding."))
+    if length(targets) != length(scores)
+        throw(DimensionMismatch("Inconsistent lengths of `targets` and `scores`."))
+    end
+    if !check_encoding(enc, targets)
+        throw(ArgumentError("`targets` vector uses incorrect label encoding."))
+    end
 
     scores_pos = sort(scores[ispositive.(enc, targets)]; rev = false)
     rates = round.(1 .- reverse(tpr); digits = 14)
-    ts, print_warn = threshold_at_rate(enc, scores_pos, rates; rev = false)
+    ts, print_warn = threshold_at_rate(scores_pos, rates; rev = false)
 
     if any(print_warn)
         rates = tpr[reverse(print_warn)]
@@ -101,22 +122,39 @@ end
 
 Returns a decision threshold at a given true negative rate `fpr ∈ [0, 1]`.
 """
-threshold_at_tnr(targets::AbstractVector, scores::RealVector, tnr) =
-    threshold_at_tnr(current_encoding(), targets, scores, tnr)
+function threshold_at_tnr(targets::AbstractVector, scores::RealVector, tnr)
+    return threshold_at_tnr(current_encoding(), targets, scores, tnr)
+end
 
 
-threshold_at_tnr(enc::TwoClassEncoding, targets::AbstractVector, scores::RealVector, tnr::Real) =
-    threshold_at_tnr(enc, targets, scores, [tnr])[1]
+function threshold_at_tnr(
+    enc::TwoClassEncoding,
+    targets::AbstractVector,
+    scores::RealVector,
+    tnr::Real,
+)
+
+    return threshold_at_tnr(enc, targets, scores, [tnr])[1]
+end
 
 
-function threshold_at_tnr(enc::TwoClassEncoding, targets::AbstractVector, scores::RealVector, tnr::RealVector)
+function threshold_at_tnr(
+    enc::TwoClassEncoding,
+    targets::AbstractVector,
+    scores::RealVector,
+    tnr::RealVector,
+)
 
-    length(targets) == length(scores) || throw(DimensionMismatch("Inconsistent lengths of `targets` and `scores`."))
-    check_encoding(enc, targets) || throw(ArgumentError("`targets` vector uses incorrect label encoding."))
+    if length(targets) != length(scores)
+        throw(DimensionMismatch("Inconsistent lengths of `targets` and `scores`."))
+    end
+    if !check_encoding(enc, targets)
+        throw(ArgumentError("`targets` vector uses incorrect label encoding."))
+    end
 
     scores_neg = sort(scores[isnegative.(enc, targets)]; rev = true)
     rates = round.(1 .- reverse(tnr); digits = 14)
-    ts, print_warn = threshold_at_rate(enc, scores_neg, rates; rev = true)
+    ts, print_warn = threshold_at_rate(scores_neg, rates; rev = true)
 
     if any(print_warn)
         rates = tnr[reverse(print_warn)]
@@ -131,21 +169,37 @@ end
 
 Returns a decision threshold at a given false positive rate `fpr ∈ [0, 1]`.
 """
-threshold_at_fpr(targets::AbstractVector, scores::RealVector, fpr) =
-    threshold_at_fpr(current_encoding(), targets, scores, fpr)
+function threshold_at_fpr(targets::AbstractVector, scores::RealVector, fpr)
+    return threshold_at_fpr(current_encoding(), targets, scores, fpr)
+end
 
 
-threshold_at_fpr(enc::TwoClassEncoding, targets::AbstractVector, scores::RealVector, fpr::Real) =
-    threshold_at_fpr(enc, targets, scores, [fpr])[1]
+function threshold_at_fpr(
+    enc::TwoClassEncoding,
+    targets::AbstractVector,
+    scores::RealVector,
+    fpr::Real,
+)
 
+    return threshold_at_fpr(enc, targets, scores, [fpr])[1]
+end
 
-function threshold_at_fpr(enc::TwoClassEncoding, targets::AbstractVector, scores::RealVector, fpr::RealVector)
+function threshold_at_fpr(
+    enc::TwoClassEncoding,
+    targets::AbstractVector,
+    scores::RealVector,
+    fpr::RealVector,
+)
 
-    length(targets) == length(scores) || throw(DimensionMismatch("Inconsistent lengths of `targets` and `scores`."))
-    check_encoding(enc, targets) || throw(ArgumentError("`targets` vector uses incorrect label encoding."))
+    if length(targets) != length(scores)
+        throw(DimensionMismatch("Inconsistent lengths of `targets` and `scores`."))
+    end
+    if !check_encoding(enc, targets)
+        throw(ArgumentError("`targets` vector uses incorrect label encoding."))
+    end
 
     scores_neg = sort(scores[isnegative.(enc, targets)]; rev = true)
-    ts, print_warn = threshold_at_rate(enc, scores_neg, fpr; rev = true)
+    ts, print_warn = threshold_at_rate(scores_neg, fpr; rev = true)
 
     if any(print_warn)
         rates = fpr[print_warn]
@@ -160,21 +214,37 @@ end
 
 Returns a decision threshold at a given false negative rate `fnr ∈ [0, 1]`.
 """
-threshold_at_fnr(targets::AbstractVector, scores::RealVector, fnr) =
-    threshold_at_fnr(current_encoding(), targets, scores, fnr)
+function threshold_at_fnr(targets::AbstractVector, scores::RealVector, fnr)
+    return threshold_at_fnr(current_encoding(), targets, scores, fnr)
+end
+
+function threshold_at_fnr(
+    enc::TwoClassEncoding,
+    targets::AbstractVector,
+    scores::RealVector,
+    fnr::Real,
+)
+
+    return threshold_at_fnr(enc, targets, scores, [fnr])[1]
+end
 
 
-threshold_at_fnr(enc::TwoClassEncoding, targets::AbstractVector, scores::RealVector, fnr::Real) =
-    threshold_at_fnr(enc, targets, scores, [fnr])[1]
+function threshold_at_fnr(
+    enc::TwoClassEncoding,
+    targets::AbstractVector,
+    scores::RealVector,
+    fnr::RealVector,
+)
 
-
-function threshold_at_fnr(enc::TwoClassEncoding, targets::AbstractVector, scores::RealVector, fnr::RealVector)
-
-    length(targets) == length(scores) || throw(DimensionMismatch("Inconsistent lengths of `targets` and `scores`."))
-    check_encoding(enc, targets) || throw(ArgumentError("`targets` vector uses incorrect label encoding."))
+    if length(targets) != length(scores)
+        throw(DimensionMismatch("Inconsistent lengths of `targets` and `scores`."))
+    end
+    if !check_encoding(enc, targets)
+        throw(ArgumentError("`targets` vector uses incorrect label encoding."))
+    end
 
     scores_pos = sort(scores[ispositive.(enc, targets)]; rev = false)
-    ts, print_warn = threshold_at_rate(enc, scores_pos, fnr; rev = false)
+    ts, print_warn = threshold_at_rate(scores_pos, fnr; rev = false)
 
     if any(print_warn)
         rates = fnr[print_warn]
@@ -191,7 +261,10 @@ Returns a decision threshold at `k` most anomalous samples if `rev == true` and
 a decision threshold at `k` least anomalous samples otherwise.
 """
 function threshold_at_k(scores::RealVector, k::Int; rev::Bool = true)
+
     n = length(scores)
-    n >= k || throw(ArgumentError("Argument `k` must be smaller or equal to `length(targets) = $n`"))
+    if n < k
+        throw(ArgumentError("`k` must be smaller or equal to `length(targets) = $(n)`"))
+    end
     return partialsort(scores, k, rev = rev)
 end
